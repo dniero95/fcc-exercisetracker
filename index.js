bf14cc7454172e11a46d4becd9056b0ca029cf08
dniero95 <darioniero@icloud.com>
2023-01-25 19:52:19 +0100

    add /api/users endpoint

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+
+
 const cors = require('cors')
 require('dotenv').config()
+const User = require('./models/user');
+
+// add body parser
+const bodyParser = require('body-parser');
+// bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +19,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+app.post('/api/users', (req, res) =>{
+  const username = req.body.username;
+  const user = new User({
+    username:username
+  })
+  res.json(user);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
